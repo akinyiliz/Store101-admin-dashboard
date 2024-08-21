fa263865deb900cd7e106d24251904946fb99510
akinyiliz <lizzieke84@gmail.com>
2024-08-21 12:50:48 +0300

    feat: Create Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import { useAuth } from "../context/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
+
+function Home() {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/dashboard");
+    }
+  }, [isAuthenticated, navigate]);
+
+  return (
+    <div className="flex flex-col items-center justify-center my-40 gap-2">
+      <h1 className="text-3xl">Welcome to Store Admin</h1>
+
+      <div className="flex gap-4">
+        <Link
+          to={"/register"}
+          className="font-medium px-8 py-2 rounded-lg bg-primaryColor text-white"
+        >
+          Register
+        </Link>
+
+        <Link
+          to={"/login"}
+          className="font-medium px-8 py-2 rounded-lg bg-primaryColor text-white"
+        >
+          Login
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
